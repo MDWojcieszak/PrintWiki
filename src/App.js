d7e827939598be32970393f7512ccf8b761576b2
MDWojcieszak <wojcieszak.4@gmail.com>
2020-05-17 15:23:50 +0200

    complited side-nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import NavBar from "./components/NavBar";
 import SideNav from "./components/SideNav";
 
 function App() {
+  const [p, setP] = React.useState(false);
+  const [ps, setPs] = React.useState(false);
+  const [f, setF] = React.useState(false);
+  const [fs, setFs] = React.useState(false);
+  const [ru, setRu] = React.useState(false);
   const [darkMode, setDarkMode] = React.useState(getInitialMode);
   React.useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -13,17 +18,53 @@ function App() {
     const mode = JSON.parse(localStorage.getItem("darkMode"));
     return mode || false;
   }
+  function onClick(e) {
+    switch (e.currentTarget.id) {
+      case "0":
+        setRu(!ru);
+        break;
+      case "1":
+        setP(true);
+        setPs(false);
+        setF(false);
+        setFs(false);
+        break;
+      case "2":
+        setP(false);
+        setPs(true);
+        setF(false);
+        setFs(false);
+        break;
+      case "3":
+        setP(false);
+        setPs(false);
+        setF(true);
+        setFs(false);
+        break;
+      case "4":
+        setP(false);
+        setPs(false);
+        setF(false);
+        setFs(true);
+        break;
+    }
+  }
   return (
     <div className={darkMode ? "App dark-mode" : "App light-mode"}>
       <NavBar
         onChange={() => setDarkMode((prevMode) => !prevMode)}
         darkMode={darkMode}
       />
-      <div className="container">
-        <SideNav />
-        <button className="btn btn-submit">SUBMIT</button>
-        <button className="btn btn-default">Kliknij mnie</button>
-      </div>
+      <SideNav
+        onClick={onClick}
+        printers={p}
+        printersSettings={ps}
+        filaments={f}
+        filamentsSettings={fs}
+        rolledUp={ru}
+      />
+      <button className="btn btn-submit">SUBMIT</button>
+      <button className="btn btn-default">Kliknij mnie</button>
     </div>
   );
 }
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,33 +1,57 @@
 import React from "react";
 import "../style/SideNav.scss";
 
-const SideNav = () => {
+const SideNav = (props) => {
   return (
-    <div className="side-nav">
+    <div
+      className={props.rolledUp ? "rolled-up side-nav" : "developed side-nav"}
+    >
       <div className="side-nav-text">
-        <div className="logo">
-          <img src={require("../img/logo.svg")} />
+        <div className="top">
+          <img className="logo" src={require("../img/logo.svg")} />
+          <img
+            id="0"
+            onClick={(e) => props.onClick(e)}
+            className="bars"
+            src={require("../img/bars.svg")}
+          />
         </div>
         <ul>
-          <li className="selected">
+          <li
+            className={props.printers ? "selected" : "not-selected"}
+            id="1"
+            onClick={(e) => props.onClick(e)}
+          >
             <span className="title">PRINTERS</span>
             <span className="icon">
               <img src={require("../img/printer.svg")} />
             </span>
           </li>
-          <li>
+          <li
+            className={props.printersSettings ? "selected" : "not-selected"}
+            id="2"
+            onClick={(e) => props.onClick(e)}
+          >
             <span className="title">PRINTERS SETTINGS</span>
             <span className="icon">
               <img src={require("../img/printer_settings.svg")} />
             </span>
           </li>
-          <li>
+          <li
+            className={props.filaments ? "selected" : "not-selected"}
+            id="3"
+            onClick={(e) => props.onClick(e)}
+          >
             <span className="title">FILAMENTS</span>
             <span className="icon">
               <img src={require("../img/filament.svg")} />
             </span>
           </li>
-          <li>
+          <li
+            className={props.filamentsSettings ? "selected" : "not-selected"}
+            id="4"
+            onClick={(e) => props.onClick(e)}
+          >
             <span className="title">FILAMENTS SETTINGS</span>
             <span className="icon">
               <img src={require("../img/filament_settings.svg")} />
